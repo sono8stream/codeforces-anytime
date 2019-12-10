dcaae80ddd221ebb87fe517d012a0cacbc6a0b11
sono8stream <sono.stream8580@gmail.com>
2019-12-10 23:10:54 +0900

    Add function fetching contest list

---
+++ functions/src/addContestList.ts
@@ -0,0 +1,58 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import fetch from 'node-fetch';
+import UncheckedContests from './types/uncheckedContests';
+
+interface ContestListResponse {
+  status: string;
+  result: ContestData[];
+}
+
+interface ContestData {
+  id: number;
+  startTimeSeconds: number;
+  relativeTimeSeconds: number;
+}
+
+export const addContestList = functions.pubsub
+  .schedule('every monday 09:00')
+  .onRun(async (context) => {
+    const doc = admin
+      .firestore()
+      .collection('availableContests')
+      .doc('uncheckedContests');
+    const snapshot = await doc.get();
+    const data = snapshot.data() as UncheckedContests;
+    const lastUpdateTime = data.lastUpdateTime;
+    const contests = await fetchContestList();
+    const contestIDs = getAddableContests(contests, lastUpdateTime);
+    const updateTime = Math.floor(new Date().getTime() / 1000);
+    await doc.update({
+      contestIDs: [...data.contestIDs, ...contestIDs],
+      lastUpdateTime: updateTime,
+    });
+    return null;
+  });
+
+const fetchContestList = async () => {
+  const url = 'https://codeforces.com/api/contest.list';
+  const response = await fetch(url);
+  const contestListResponse = (await response.json()) as ContestListResponse;
+  return contestListResponse.result;
+};
+
+const getAddableContests = (
+  contests: ContestData[],
+  lastUpdateTime: number
+) => {
+  const contestIDs = [];
+  for (const contest of contests) {
+    if (
+      contest.relativeTimeSeconds >= 0 &&
+      contest.startTimeSeconds > lastUpdateTime
+    ) {
+      contestIDs.push(contest.id);
+    }
+  }
+  return contestIDs;
+};
