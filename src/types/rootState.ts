26239f0069aaec4241ddb856c1494f281f4d3cfb
sono8 <sono.stream8580@gmail.com>
2020-05-03 00:13:10 +0900

    Add contest list types

--- src/types/rootState.ts
+++ src/types/rootState.ts
@@ -1,7 +1,11 @@
 import AccountInfo from './accountInfo';
 import UserProfile from './userProfile';
+import AvailableContestInfo from './availableContestInfo';
+import ContestRecord from './contestRecord';
 
 export default interface RootState {
-  profileReducer: UserProfile;
-  accountReducer: AccountInfo;
+  profile: UserProfile;
+  availableContests: AvailableContestInfo[];
+  officialRatingRecords: ContestRecord[];
+  account: AccountInfo;
 }
